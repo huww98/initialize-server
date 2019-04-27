79b50e287cc22a984f860b86fbfa955eb6efa373
胡玮文 <huww98@outlook.com>
2019-04-27 21:15:27 +0800

    auto detect disk device filename default target nvidia driver to offical CUDA repo bug fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Set Timezone
 # Format and mount all disks as follow:
 # NAME                      SIZE TYPE  MOUNTPOINT
 # sda                     223.6G disk
@@ -26,37 +25,47 @@
 
 set -e
 
-if pvs | grep '/dev/sd[cd]'
-then
-    echo "HDD already used as LVM PV"; exit 1;
+LSBLK_OUT=$(lsblk --noheadings --output SIZE,NAME,TYPE --raw --bytes --paths | sort --general-numeric-sort --stable | grep disk)
+if [[ $(wc -l <<<"$LSBLK_OUT") != "4" ]]; then
+    echo "Not 4 disk detected"; exit 1
 fi
+SSD_SYS=$(awk 'NR==1 {print $2}' <<<"$LSBLK_OUT")
+SSD_2=$(awk 'NR==2 {print $2}' <<<"$LSBLK_OUT")
+HDD_1=$(awk 'NR==3 {print $2}' <<<"$LSBLK_OUT")
+HDD_2=$(awk 'NR==4 {print $2}' <<<"$LSBLK_OUT")
 
-for sd in /dev/sd{a,b,c,d}
+DISKS=($(awk '{print $2}' <<<"$LSBLK_OUT"))
+HDD=("$HDD_1" "$HDD_2")
+HDD_PARTS=("${HDD_1}1" "${HDD_2}1")
+
+for h in "${HDD[@]}"
+do
+    if pvs | grep 'h'
+    then
+        echo "HDD already used as LVM PV"; exit 1;
+    fi
+done
+
+for sd in "${DISKS[@]}"
 do
     fdisk -l $sd | grep -q "Disklabel type: gpt" || { echo "$sd disklabel type is not GPT"; exit 1; }
 done
 
-sed --in-place "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/" /etc/apt/sources.list
-apt-get update
-apt-get upgrade
-
-timedatectl set-timezone Asia/Shanghai
-
 lvextend ubuntu-vg/ubuntu-lv --extents +100%FREE
 resize2fs /dev/ubuntu-vg/ubuntu-lv
 
-sfdisk --part-type /dev/sdb 1 E6D6D379-F507-44C2-A23C-238F2A3DF928 # Linux LVM
-pvcreate /dev/sdb1 -y
-vgcreate ssd-vg /dev/sdb1
+sfdisk --part-type ${SSD_2} 1 E6D6D379-F507-44C2-A23C-238F2A3DF928 # Linux LVM
+pvcreate "${SSD_2}1" -y
+vgcreate ssd-vg "${SSD_2}1"
 lvcreate ssd-vg --name ssd-lv --extents 100%VG
 
 mdadm --stop /dev/md0 || true
-for sd in /dev/sd{c,d}
+for sd in "${HDD[@]}"
 do
     sfdisk --part-type $sd 1 A19D880F-05FC-4D3B-A006-743F0F84911E # Linux RAID
 done
 mdadm --stop /dev/md0 || true
-mdadm --create /dev/md/hdd --level raid0 --raid-devices=2 /dev/sdc1 /dev/sdd1
+mdadm --create /dev/md/hdd --level raid0 --raid-devices=2 "${HDD_PARTS[@]}"
 pvcreate /dev/md/hdd
 vgcreate hdd-vg /dev/md/hdd
 lvcreate hdd-vg --name dataset-lv --size 3T
@@ -68,7 +77,7 @@ mkfs.xfs /dev/hdd-vg/dataset-lv
 
 cat > /etc/fstab << EOF
 UUID=$(blkid /dev/ubuntu-vg/ubuntu-lv -o value | head -n 1) / ext4 defaults 0 1
-UUID=$(blkid /dev/sda2 -o value | head -n 1) /boot ext4 defaults 0 2
+UUID=$(blkid ${SSD_SYS}2 -o value | head -n 1) /boot ext4 defaults 0 2
 /swap.img       none    swap    sw      0 0
 UUID=$(blkid /dev/ssd-vg/ssd-lv -o value | head -n 1) /mnt/ssd ext4 defaults 0 2
 UUID=$(blkid /dev/hdd-vg/home-lv -o value | head -n 1) /home ext4 defaults 0 2
